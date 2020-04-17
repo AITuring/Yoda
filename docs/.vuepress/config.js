f1b7d43546671905edd47e502e88bd411e581995
AITuring <shspku365@gmail.com>
2020-04-18 05:56:35 +0800

    ico

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,9 @@
 module.exports = {
     title: 'Yoda', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
     description: '一个MVVM框架',// meta 中的描述文字，用于SEO
+    head: [
+        ['link', { rel: 'shortcut icon', type: "image/x-icon", href: `./fav.ico` }]
+     ],
     themeConfig: {
         logo: '/jedi.jpg',
         repo: 'https://github.com/AITuring/Yoda',
